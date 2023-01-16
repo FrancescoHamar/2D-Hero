5bdfd37d60b56bc88ca16f483e714d3ade7885d5
FrancescoHamar <92935796+FrancescoHamar@users.noreply.github.com>
2023-01-16 11:24:16 +0100

    Project Commit

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject player;
+
+    public Grounded groundedScript;
+
+    public Messenger messenger;
+
+    public int levelNumber;
+
+    void Start()
+    {
+        groundedScript = GameObject.Find("Player").GetComponent<Grounded>();
+        messenger = GameObject.Find("Messenger").GetComponent<Messenger>();
+    }
+
+    public void restart()
+    {
+        groundedScript.menuAnimator.SetBool("isDead", false);
+        groundedScript.animator.SetBool("isDead", false);
+        player.transform.position = groundedScript.originalPos;
+    }
+
+    public void toMainMenu()
+    {
+        groundedScript.menuAnimator.SetBool("isDead", false);
+        groundedScript.animator.SetBool("isDead", false);
+        player.transform.position = groundedScript.originalPos;
+        SceneManager.LoadScene(0);
+    }
+
+    public void toLevelMenu()
+    {
+        groundedScript.menuAnimator.SetBool("isDead", false);
+        groundedScript.animator.SetBool("isDead", false);
+        player.transform.position = groundedScript.originalPos;
+        if (messenger.currentLevel < 12)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (messenger.currentLevel < 23)
+        {
+            SceneManager.LoadScene(12);
+        }
+        else
+        {
+            SceneManager.LoadScene(23);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/optionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionsMenu : MonoBehaviour
+{
+    public Animator animator;
+    
+    public void OpenOptions()
+    {
+        animator.SetBool("mainOptions", true);
+
+    }
+
+    public void CloseOptions()
+    {
+        animator.SetBool("mainOptions", false);
+    }
+
+
+}
